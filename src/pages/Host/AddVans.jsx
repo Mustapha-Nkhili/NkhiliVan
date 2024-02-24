7650843422d5b03ea0a4f7dfc954b6719e4ac9f5
Mustapha-Nkhili <mustapha.developer.23@gmail.com>
2024-02-24 22:45:30 +0100

    refactor: Enhance image selection, input validation, and UI for selected van image in AddVans component

--- src/pages/Host/AddVans.jsx
+++ src/pages/Host/AddVans.jsx
@@ -1,8 +1,60 @@
+import { useState } from "react";
+import vanImgPlaceholder from "../../assets/imgs/van-img-placeholder.png";
+
 const AddVans = () => {
+  const [imgTypeError, setImgTypeError] = useState(null);
+  const [vanImg, setVanImg] = useState(null);
+
+  const onImgSelected = (e) => {
+    const file = e.target.files[0];
+    if (file.type && !file.type.startsWith("image/")) {
+      setImgTypeError(true);
+      return;
+    }
+
+    setImgTypeError(null);
+    const reader = new FileReader();
+    reader.addEventListener("load", (e) => {
+      setVanImg(e.target.result);
+    });
+    reader.addEventListener("error", (e) => {
+      console.error("Error occurred while reading the file:", e.target.error);
+    });
+
+    reader.readAsDataURL(file);
+  };
+
+  const handleVanTypeCLicked = (e) => {
+    e.target.querySelector('input[type="radio"]').checked = true;
+  };
+
   return (
     <section className="add-vans container">
       <h1>Add new Van</h1>
-      <input type="file" name="van-img" id="vanImgInput" accept="image/*" />
+      <label className="choose-van-img">
+        <div className="img-container" style={{ padding: vanImg && "0px" }}>
+          <img
+            src={vanImg ? vanImg : vanImgPlaceholder}
+            style={vanImg && { width: "100%", height: "100%" }}
+            alt="this is the van img placeholder picture"
+          />
+        </div>
+        <p>Click to upload image</p>
+        <span>JPG, PNG, SVG (2MB max)</span>
+        {imgTypeError && (
+          <span className="img-type-error">
+            Sorry, we couldn't process your file. It appears that the file you
+            uploaded is not an image or its format is not supported.
+          </span>
+        )}
+        <input
+          type="file"
+          name="van-img"
+          id="vanImgInput"
+          accept=".png, .jpg, .jpeg, .svg"
+          onChange={onImgSelected}
+        />
+      </label>
 
       <input
         type="text"
@@ -26,27 +78,21 @@ const AddVans = () => {
 
       <h2>Choose item type</h2>
       <ul className="vans-type-list">
-        <li
-          onClick={(e) =>
-            (e.target.querySelector('input[type="radio"]').checked = true)
-          }
-        >
+        <li onClick={handleVanTypeCLicked}>
           <label htmlFor="simple">simple</label>
-          <input type="radio" name="van-type" id="simple" value={"simple"} />
+          <input
+            type="radio"
+            name="van-type"
+            id="simple"
+            value={"simple"}
+            checked
+          />
         </li>
-        <li
-          onClick={(e) =>
-            (e.target.querySelector('input[type="radio"]').checked = true)
-          }
-        >
+        <li onClick={handleVanTypeCLicked}>
           <label htmlFor="rugged">rugged</label>
           <input type="radio" name="van-type" id="rugged" value={"rugged"} />
         </li>
-        <li
-          onClick={(e) =>
-            (e.target.querySelector('input[type="radio"]').checked = true)
-          }
-        >
+        <li onClick={handleVanTypeCLicked}>
           <label htmlFor="luxury">luxury</label>
           <input type="radio" name="van-type" id="luxury" value={"luxury"} />
         </li>
